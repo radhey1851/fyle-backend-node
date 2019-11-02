ad81689a7e9fa62432297638605729ca59a09c59
Radheshyam Etikala <radhey1851@gmail.com>
2019-11-03 02:03:03 +0530

    Add curl test scripts

---
+++ curl scripts/branches.sh
@@ -0,0 +1,3 @@
+curl \
+  -H "Authorization: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InJhZGhleSIsImlhdCI6MTU3MjcyNDQ5NCwiZXhwIjoxNTczMTU2NDk0fQ.bEBDOI3AbHnqwD3r5sx6eDlVJrpRkcJHorQCcOJ-GMA" \
+  https://fyle-backend-test.herokuapp.com/api/banks/UNITED%20BANK%20OF%20INDIA/city/KOLKATA?limit=3&offset=2
---
+++ curl scripts/ifsc.sh
@@ -0,0 +1,3 @@
+curl \
+  -H "Authorization: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InJhZGhleSIsImlhdCI6MTU3MjcyNDQ5NCwiZXhwIjoxNTczMTU2NDk0fQ.bEBDOI3AbHnqwD3r5sx6eDlVJrpRkcJHorQCcOJ-GMA" \
+  https://fyle-backend-test.herokuapp.com/api/branches/UTBI0TUA857
