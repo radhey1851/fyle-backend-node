be016ebbef40113412824781465f6cbb84bf4487
Radheshyam Etikala <radhey1851@gmail.com>
2019-11-03 00:53:14 +0530

    Change token expiry to 5 days

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -5,7 +5,7 @@ module.exports = async (req, res) => {
   let token;
 
   if (password === process.env.TEST_PASSWORD) {
-    token = jwt.sign(username, process.env.TOKEN_SECRET);
+    token = jwt.sign(username, process.env.TOKEN_SECRET, { expiresIn: '5d' });
     res.status(200).send({ token });
   } else {
     res.status(400).send('Invalid password');
