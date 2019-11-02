a4afa02162147b4f85a2ba78771faee712c8c25d
Radheshyam Etikala <radhey1851@gmail.com>
2019-11-03 01:19:20 +0530

    Fix typos in request header values

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -6,7 +6,7 @@ module.exports = async (req, res) => {
 
   if (password === process.env.TEST_PASSWORD) {
     token = jwt.sign({ username }, process.env.TOKEN_SECRET, { expiresIn: '5d' });
-    res.status(200).send({ token });
+    res.status(200).send({ 'x-auth-token': token });
   } else {
     res.status(400).send('Invalid password');
   }
--- src/middlware/auth.js
+++ src/middlware/auth.js
@@ -1,10 +1,11 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
-  const token = req.headers.Authorization;
-
+  const token = req.headers['authorization'];
+  console.log(req.headers);
   jwt.verify(token, process.env.TOKEN_SECRET, (err) => {
     if (err) {
+      console.log(err);
       res.status(403).send('Invalid authorization credentials');
     } else {
       next();
